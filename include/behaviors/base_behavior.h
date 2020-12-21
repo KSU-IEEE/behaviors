99e6847855ff8419231fd5344d8da3f745adee6c
Trevor Stanca <tstanca@students.kennesaw.edu>
2020-12-21 13:26:18 -0500

    updated behavior with new subscirber for sm and init and deinit functions

--- include/behaviors/base_behavior.h
+++ include/behaviors/base_behavior.h
@@ -1,26 +1,33 @@
 #ifndef BASE_BEHAVIOR_H
 #define BASE_BEHAVIOR_H
 #include <ros/ros.h>
+#include <std_msgs/Bool.h>
 #include <string.h>
 
 using namespace std;
 namespace behaviors {
 class base_behavior {
 public:
+    // init and deinit
+    base_behavior();
+    base_behavior(string node_name);
+    ~base_behavior();
+
     // virtual functions
     virtual bool control_loop();
     virtual void set_params();
-
+    virtual void init();
+    
     void set_nh(ros::NodeHandle nh);
     ros::NodeHandle nh();
     string node_name();
+    void set_node_name(string name);
     bool in_control();
+
+    // ros callbacks
+    void activate_cb(const std_msgs::Bool::ConstPtr& activate);
 private:
     string name_ = "base";
-    /*
-    TODO:
-    Need to set basic subscriber to see if in control
-    */
     bool in_control_ = true;
 protected:
     ros::NodeHandle nh_;   
--- src/base_behavior.cpp
+++ src/base_behavior.cpp
@@ -1,16 +1,31 @@
 #include <behaviors/base_behavior.h>
 namespace behaviors {
+base_behavior::base_behavior() {}
+base_behavior::base_behavior(string node_name) {
+    set_node_name(node_name);
+}
+base_behavior::~base_behavior() {}
+
 void base_behavior::set_nh(ros::NodeHandle nh) {
     nh_ = nh;
 }
 ros::NodeHandle base_behavior::nh() {
     return nh_;
 }
+
 string base_behavior::node_name() {
     return name_;
 }
+void base_behavior::set_node_name(string name) {
+    name_ = name;
+}
+
 bool base_behavior::in_control() {
     return in_control_;
+}
+
+void base_behavior::activate_cb(const std_msgs::Bool::ConstPtr& activate) {
+    in_control_ = activate->data;
 }
 
 // to be called by each rosnode. set beh to the new behavior class 
@@ -27,16 +42,16 @@ int main(int argc, char* argv[])
     ros::NodeHandle nh;
     beh_.set_nh(nh);
     
-    // grab params from launch files
+    // initialize subscribers && grab params from launch files
+    beh_.init();
     beh_.set_params();
 
-    /*
-    TODO
-    Create something to create a publisher or subscriber 
-    to to run `control_loop` when the state machine activates it
-    */
+    // setup route to activate with sm 
+    string topic_name = beh_.node_name() + "/activate";
+    ros::Subscriber sm_sub = nh.subscribe(topic_name, 1000, beh_.activate_cb);
+
     while(nh.ok()) {
-        beh_.control_loop();
+        if (beh_.in_control()) beh_.control_loop();
     }
 }
 } // namespace behaviors
